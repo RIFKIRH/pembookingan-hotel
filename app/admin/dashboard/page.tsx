1f172e342d30de480035be14dd188e32406134af
RIFKIRH <rifkirahmatunh@gmail.com>
2026-01-13 19:16:41 +0700

    dashboard admin

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { Metadata } from "next";
+import DashboardCards from "@/components/admin/dashboard-cards";
+import { Suspense } from "react";
+import ReservationList from "@/components/admin/reservation-list";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
 
 const DashboardPage = () => {
-  return <div className="py-20 mt-10">Dashboard Page</div>;
+  return (
+    <div className="max-x-screen-xl px-4 py-16 mt-10 mx-auto">
+      <h1 className="text-4xl font-bold text-gray-800">Dashboard</h1>
+      <Suspense fallback={<p>Loading Cards....</p>}>
+        <DashboardCards />
+      </Suspense>
+      <Suspense fallback={<p>Loading Reservation....</p>}>
+        <ReservationList />
+      </Suspense>
+    </div>
+  );
 };
 
 export default DashboardPage;
